799a6f1a8202b7c29eb06f9a5e1a4b32b0849785
Jay Paygude <jaypaygude2@gmail.com>
2025-08-06 16:12:16 +0530

    Create matrixaddition.cpp

---
+++ matrixaddition.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows1, cols1, rows2, cols2;
+
+    cout << "Enter number of rows for Matrix 1: ";
+    cin >> rows1;
+    cout << "Enter number of columns for Matrix 1: ";
+    cin >> cols1;
+
+    cout << "Enter number of rows for Matrix 2: ";
+    cin >> rows2;
+    cout << "Enter number of columns for Matrix 2: ";
+    cin >> cols2;
+
+    if (rows1 != rows2 || cols1 != cols2) {
+        cout << "Error: Matrices must have the same dimensions for addition.\n";
+        return 1;
+    }
+
+    int matrix1[rows1][cols1], matrix2[rows2][cols2], sum[rows1][cols1];
+
+    cout << "Enter elements of Matrix 1:\n";
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            cout << "Element at [" << i << "][" << j << "]: ";
+            cin >> matrix1[i][j];
+        }
+    }
+    cout << "Enter elements of Matrix 2:\n";
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            cout << "Element at [" << i << "][" << j << "]: ";
+            cin >> matrix2[i][j];
+        }
+    }
+    cout << "\nMatrix 1:\n";
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            cout << matrix1[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "\nMatrix 2:\n";
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            cout << matrix2[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "\nSum of Matrix 1 and Matrix 2:\n";
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            cout << sum[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    return 0;
+}
